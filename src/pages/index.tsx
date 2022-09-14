51a70d888856b3a4afe58b539ffbd3757ea435fe
EddyPBR <edvaldojunior1310@gmail.com>
2022-09-14 15:03:21 -0300

    :wrench: config tailwindcss for nextjs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <main>
-      <h1>NLW e-Sports</h1>
+      <h1 className="text-xl">NLW e-Sports</h1>
     </main>
   );
 };
