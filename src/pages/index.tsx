4a8115d9ea9f875f9a5e6066aaffdcdd236bc5bb
EddyPBR <edvaldojunior1310@gmail.com>
2022-09-17 11:33:26 -0300

    :hammer: refact landing.page with components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from "next";
 import NextImage from "next/image";
 
 import { MagnifyingGlassPlus } from "phosphor-react";
+import { CreateAdBanner } from "~components/CreateAdBanner";
+import { GameBanner } from "~components/GameBanner";
 
 const Home: NextPage = () => {
   return (
@@ -25,35 +27,16 @@ const Home: NextPage = () => {
         {Array(6)
           .fill(null)
           .map((_, index) => (
-            <a key={index + 1} href="#" className="relative rounded-lg overflow-hidden">
-              <NextImage
-                src={`/images/image%20${index + 1}.png`}
-                alt={`Capa do jogo game-${index + 1}`}
-                width={180}
-                height={240}
-              />
-
-              <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-1.5 left-0 right-0 rounded-md">
-                <strong className="font-bold text-white block">Game {index + 1}</strong>
-                <span className="text-zinc-300 text-sm block">{index + 2} anúncios</span>
-              </div>
-            </a>
+            <GameBanner
+              adsCount={index + 2}
+              bannerUrl={`/images/image%20${index + 1}.png`}
+              title={`Game ${index + 1}`}
+              key={index + 1}
+            />
           ))}
       </section>
 
-      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden ">
-        <article className="bg-[#2A2634] px-8 py-6 flex justify-between">
-          <div>
-            <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
-            <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
-          </div>
-
-          <button className="h-full py-3 px-4 bg-violet-500 text-white rounded flex items-center gap-3 hover:bg-violet-600 transition-colors">
-            <MagnifyingGlassPlus size={24} />
-            Publicar anúncio
-          </button>
-        </article>
-      </div>
+      <CreateAdBanner />
     </main>
   );
 };
