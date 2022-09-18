ab92edf1f145024f199dd648342c8a0436a72bbb
EddyPBR <edvaldojunior1310@gmail.com>
2022-09-18 00:55:23 -0300

    :wrench: enable new fonts in _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
 
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;900&display=swap"
           rel="stylesheet"
         />
       </Head>
