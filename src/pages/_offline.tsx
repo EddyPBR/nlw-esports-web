0dd51744f168ab7c2abc14497f9623312ac5f4b0
EddyPBR <edvaldojunior1310@gmail.com>
2022-09-19 22:07:56 -0300

    :sparkles: implements offline page

---
+++ src/pages/_offline.tsx
@@ -0,0 +1,32 @@
+import type { NextPage } from "next";
+
+import NextLink from "next/link";
+
+const OfflinePage: NextPage = () => {
+  return (
+    <div className="h-screen w-full flex flex-col justify-center">
+      <strong className="bg-nlw-gradient text-transparent bg-clip-text text-9xl font-black mx-auto">
+        Oops!
+      </strong>
+
+      <main className="max-w-lg w-full mx-auto flex flex-col gap-2 mt-2">
+        <h1 className="mx-auto mt-6 text-xl text-white font-bold text-center">
+          Você esta offline
+        </h1>
+
+        <p className="mx-auto text-gray-300 text-center">
+          Impossível carregar, você está offline cheque sua conexão com a
+          internet.
+        </p>
+
+        <NextLink href="/" passHref>
+          <a className="mt-6 py-3 px-4 text-white font-medium rounded bg-violet-500 hover:bg-violet-600 transition-colors w-36 mx-auto">
+            Voltar ao inicio
+          </a>
+        </NextLink>
+      </main>
+    </div>
+  );
+};
+
+export default OfflinePage;
